f8fe2f5cccffc53ab47e751e3ea7e2770d06e634
Nathan Justin <nathanjustin03@gmail.com>
2024-02-27 23:17:36 -0500

    Updates from main

--- Assets/Scripts/AnimateHandOnInput.cs
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.InputSystem;
@@ -10,16 +11,18 @@ public class AnimateHandOnInput : MonoBehaviour
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
-	[SerializeField] private GameObject sword;
-	[SerializeField] private GameObject ninjaStar;
-	private bool holdingThrowable;
+    [SerializeField] private GameObject sword;
+    [SerializeField] private GameObject ninjaStar;
+    private Vector3 gripPoint;
+    private bool isGrabbed;
+    private GameObject instantiatedPrefab;
+    private float throwForce = 5.0f;
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-		holdingThrowable = false;
-
-	}
+        isGrabbed = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -29,30 +32,46 @@ public class AnimateHandOnInput : MonoBehaviour
         handAnimator.SetFloat("Trigger", triggerValue);
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
-		// both buttons need to be selected
-		handAnimator.SetFloat("Grip", gripValue);
+        // both buttons need to be selected
+        handAnimator.SetFloat("Grip", gripValue);
 
-		// active sword grip
-		if (gripValue > 0.9f)
-		{
-			if (sword == null) return;
-			sword.SetActive(true);
-		}
-		else
-		{
-			if (sword == null) return;
-			sword.SetActive(false);
-		}
+        // active sword grip
+        if (gripValue > 0.9f)
+        {
+            if (sword == null) return;
+            sword.SetActive(true);
+        }
+        else
+        {
+            if (sword == null) return;
+            sword.SetActive(false);
+        }
 
-		//active ninja star pinch
-		if (triggerValue > 0.9f && gripValue < 0.1f && !holdingThrowable)
-		{
-			GameObject instantiatedPrefab = Instantiate(ninjaStar, transform.position, transform.rotation);
-			holdingThrowable = true;
-		}
-		else
-		{
-			holdingThrowable = false;
-		}
-	}
+        //active ninja star pinch
+        if (!isGrabbed && triggerValue > 0.9f && gripValue < 0.1f && ninjaStar != null)
+        {
+            instantiatedPrefab = Instantiate(ninjaStar, transform.position, transform.rotation);
+            gripPoint = instantiatedPrefab.transform.GetChild(0).position;
+            instantiatedPrefab.transform.position = transform.position;
+            isGrabbed = true;
+
+        }
+
+        if (isGrabbed && triggerValue <= 0.01f && gripValue >= 0.9f)
+        {
+            ThrowObject();
+            Debug.Log("gonna throw");
+            isGrabbed = false;
+
+        }
+
+    }
+
+    private void ThrowObject()
+    {
+        Debug.Log("thrown");
+        Rigidbody rb = instantiatedPrefab.GetComponent<Rigidbody>();
+        //rb.isKinematic = false;
+        rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+    }
 }
--- Assets/Scripts/FruitBehavior.cs
+++ Assets/Scripts/FruitBehavior.cs
@@ -6,64 +6,86 @@ using UnityEngine;
 public class FruitBehavior : MonoBehaviour
 {
     private int hitPoints;
+    private Vector3[] strikePoints;
+    private Vector3[] strikePointsNormals;
 
-	private void OnCollisionEnter(Collision collision)
-	{
-		if(collision.collider.transform.name == "sword")
-		{
-			hitPoints -= 2;
-			Debug.Log("hit with sword");
-		}
-		if (collision.collider.transform.name == "Shuriken4")
-		{
-			hitPoints -= 1;
-			Debug.Log("hit with star");
-
-		}
-		if (hitPoints == 0) Destroy(gameObject);
-	}
-
-	// Start is called before the first frame update
-	void Start()
+    private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("hit");
+        strikePoints[0] = collision.contacts[0].point;
+        Debug.Log(collision.contactCount);
+        if (collision.collider.transform.name == "sword")
+        {
+            hitPoints -= 2;
+            Debug.Log("hit with sword");
+        }
+        if (collision.collider.transform.name == "Shuriken4")
+        {
+            hitPoints -= 1;
+            Debug.Log("hit with star");
+
+        }
+        if (hitPoints <= 0) Destroy(gameObject);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        strikePoints[1] = collision.contacts[0].point;
+
+        Debug.Log(collision.contactCount);
+        Debug.DrawLine(strikePoints[0], strikePoints[1], Color.red);
+
+
+        strikePoints = new Vector3[strikePoints.Length];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        strikePoints = new Vector3[2];
+
         switch (gameObject.name)
         {
-			case "apple":
-				hitPoints = 5;
-				break;
-			case "avocado":
-				hitPoints = 7;
-				break;
-			case "banana":
-				hitPoints = 6;
-				break;
-			case "cherries":
-				hitPoints = 8;
-				break;
-			case "lemon":
-				hitPoints = 5;
-				break;
-			case "peach":
-				hitPoints = 5;
-				break;
-			case "peanut":
-				hitPoints = 6;
-				break;
-			case "pear":
-				hitPoints = 4;
-				break;
-			case "strawberry":
-				hitPoints = 10;
-				break;
-			case "watermelon":
-				hitPoints = 10;
-				break;
-		}
-	}
+            case "apple":
+                hitPoints = 5;
+                break;
+            case "avocado":
+                hitPoints = 7;
+                break;
+            case "banana":
+                hitPoints = 6;
+                break;
+            case "cherries":
+                hitPoints = 8;
+                break;
+            case "lemon":
+                hitPoints = 5;
+                break;
+            case "peach":
+                hitPoints = 5;
+                break;
+            case "peanut":
+                hitPoints = 6;
+                break;
+            case "pear":
+                hitPoints = 4;
+                break;
+            case "strawberry":
+                hitPoints = 10;
+                break;
+            case "watermelon":
+                hitPoints = 10;
+                break;
+        }
+    }
+
+    private void DrawLine(Vector3 entryPoint, Vector3 exitPoint)
+    {
+        Debug.DrawLine(entryPoint, exitPoint);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
